0c87c255fb89bb9428e28ba7fe9000dcd1c0cfdc
RRZhai <126899363+RRZhai@users.noreply.github.com>
2023-05-23 20:50:54 -0700

    passed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return ' and '.join([items[0], items[1]])
+    elif len(items) > 2:
+        last_item = ', and '.join([items[-2], items[-1]])
+        items.pop()
+        items.pop()
+        items.append(last_item)
+        return ', '.join(items)
